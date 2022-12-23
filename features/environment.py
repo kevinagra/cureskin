50aa9413b43c5fdf9f0fa16a97a31517173d330e
Kevin <qa.kevinagra@gmail.com>
2022-12-22 19:07:11 -0800

    12/22/22: CAP_72

--- features/environment.py
+++ features/environment.py
@@ -34,7 +34,7 @@ def browser_init(context, test_case):
             "osVersion": "11",
             "browserVersion": "latest",
             "projectName": "CureSkin",
-            "buildName": "CAP_25_Reviews.feature",
+            "buildName": "CAP_72_View_All.feature",
             "local": "false",
             "networkLogs": "true",
             "seleniumVersion": "3.14.0",
